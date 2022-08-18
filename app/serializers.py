de84be5bca7dbeae57c00b29de1decee3d1dc685
AinazikS <kcajydna@gmail.com>
2022-08-18 16:26:09 +0600

    childrens

--- app/serializers.py
+++ app/serializers.py
@@ -2,6 +2,20 @@ from rest_framework import serializers
 from .models import Product, Review, UserProduct
 
 
+class FilterReviewListSerializer(serializers.ListSerializer):
+    """Фильтр комментариев, только parents"""
+    def to_representation(self, data):
+        data = data.filter(parent=None)
+        return super().to_representation(data)
+
+
+class RecursiveSerializer(serializers.Serializer):
+    """Вывод рекурсивно children"""
+    def to_representation(self, value):
+        serializer = self.parent.parent.__class__(value, context=self.context)
+        return serializer.data
+
+
 class ReviewCreateSerializer(serializers.ModelSerializer):
     """Добавление отзыва"""
 
@@ -12,10 +26,12 @@ class ReviewCreateSerializer(serializers.ModelSerializer):
 
 class ReviewSerializer(serializers.ModelSerializer):
     """Вывод отзыво"""
+    children = RecursiveSerializer(many=True)
 
     class Meta:
+        list_serializer_class = FilterReviewListSerializer
         model = Review
-        fields = ("name", "text",)
+        fields = ("name", "text","children")
 
 
 class UserProductSerializers(serializers.ModelSerializer):
@@ -25,9 +41,13 @@ class UserProductSerializers(serializers.ModelSerializer):
 
 
 class ProductDetailSerializers(serializers.ModelSerializer):
-    reviews = ReviewSerializer(many=True)
-    rates = UserProductSerializers(many=True)
-
+    # reviews = ReviewSerializer(many=True, read_only=True)
+    # rates = UserProductSerializers(many=True, read_only=True)
+    def to_representation(self, instance):
+        representation = super().to_representation(instance)
+        representation['reviews'] = ReviewSerializer(instance.reviews.all(), many=True, context=self.context).data
+        representation['rates'] = UserProductSerializers(instance.rates.all(), many=True, context=self.context).data
+        return representation
     class Meta:
         model = Product
         fields = '__all__'
